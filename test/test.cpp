b13678c8a2b6975a981f5c192dc76ba10731ded4
hetao <hetao@sensorsdata.cn>
2019-09-18 13:20:23 +0800

    add a test

---
+++ test/test.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+
+int main(){
+	int n;
+	cout<<"Please input a number:"<<endl;
+	cin>>n;
+	for(int i=0;i<=n;i++){
+		cout<<i<<endl;	
+	}
+	return 0;
+}
